7f0f36b650733a879cc96971e6132b33c1389517
GenBill <zhang478127826@gmail.com>
2021-09-27 21:10:12 +0800

    Blocker

--- runner/my_Rand.py
+++ runner/my_Rand.py
@@ -1,6 +1,9 @@
 import torch
 
 def rain_Rand(size, device):
+    # rain blocker
+    # return torch.rand(size,device)
+    
     # /3 -> z = 1.5 -> 6.68%
     rand_n = torch.randn(size, device=device)
     rand_n = torch.abs(rand_n/3)
@@ -9,6 +12,9 @@ def rain_Rand(size, device):
     return ret
 
 def tower_Rand(size, device):
+    # tower blocker
+    # return torch.rand(size,device)
+    
     # P0 = max，P1 = min
     rand_u = torch.rand(size, device=device) + torch.rand(size, device=device)
     return torch.abs(rand_u-1)

9cf0048fe99093b3f90d0f366dfba1705c350ef2
GenBill <zhang478127826@gmail.com>
2021-10-18 11:01:27 +0000

    Prime

--- bash/Final_index.sh
+++ bash/Final_index.sh
@@ -1,18 +1,21 @@
-CUDA_VISIBLE_DEVICES=5 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 21204 clean-train.py >/dev/null 2>&1
-CUDA_VISIBLE_DEVICES=4 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 21205 adv-train.py >/dev/null 2>&1
+# CUDA_VISIBLE_DEVICES=5 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 21204 clean-train.py >/dev/null 2>&1
+# CUDA_VISIBLE_DEVICES=4 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 21205 adv-train.py >/dev/null 2>&1
 
-CUDA_VISIBLE_DEVICES=1 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 212061 multar-adv-train.py >/dev/null 2>&1
-CUDA_VISIBLE_DEVICES=2 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 212062 multar-adv-train.py >/dev/null 2>&1
-CUDA_VISIBLE_DEVICES=3 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 21207 target-adv-train.py >/dev/null 2>&1
+# CUDA_VISIBLE_DEVICES=1 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 212061 multar-adv-train.py >/dev/null 2>&1
+# CUDA_VISIBLE_DEVICES=2 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 212062 multar-adv-train.py >/dev/null 2>&1
+# CUDA_VISIBLE_DEVICES=3 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port 21207 target-adv-train.py >/dev/null 2>&1
 
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port 21216 multar-adv-train.py
+# python -m torch.distributed.launch --nproc_per_node=8 --master_port 21216 multar-adv-train.py
 python -m torch.distributed.launch --nproc_per_node=8 --master_port 21204 clean-train.py
 python -m torch.distributed.launch --nproc_per_node=8 --master_port 21205 adv-train.py
 
 python -m torch.distributed.launch --nproc_per_node=8 --master_port 21226 multar-adv-train.py
 python -m torch.distributed.launch --nproc_per_node=8 --master_port 21207 target-adv-train.py
 
-CUDA_VISIBLE_DEVICES=5 python -m torch.distributed.launch --nproc_per_node=1 --master_port 21208 adv-train-MSE.py
-CUDA_VISIBLE_DEVICES=5 python -m torch.distributed.launch --nproc_per_node=1 --master_port 21207 clean-train-MSE.py
+python -m torch.distributed.launch --nproc_per_node=8 --master_port 21208 adv-train-MSE.py
+python -m torch.distributed.launch --nproc_per_node=8 --master_port 21207 clean-train-MSE.py
+
+# CUDA_VISIBLE_DEVICES=5,7 python -m torch.distributed.launch --nproc_per_node=2 --master_port 21208 adv-train-MSE.py
+# CUDA_VISIBLE_DEVICES=5,7 python -m torch.distributed.launch --nproc_per_node=2 --master_port 21207 clean-train-MSE.py
 

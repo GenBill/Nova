1203178f7a24b1107fcf65f20d8e76cab0d36c66
GenBill <zhang478127826@gmail.com>
2022-01-09 00:45:51 +0800

    device not cuda

--- attacker/fea_link.py
+++ attacker/fea_link.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 from torch.autograd.gradcheck import zero_gradients
-import ot
+# import ot
 
 def _model_freeze(model):
         for param in model.parameters():
@@ -87,9 +87,9 @@ class FeaSAttack(nn.Module):
             # logits_pred, fea = self.model(x)
             logits_pred = self.model(x)
 
-            ot_loss = ot.sinkhorn_loss_joint_IPOT(1, 0.00, logits_pred_nat,
+            ot_loss = sinkhorn_loss_joint_IPOT(1, 0.00, logits_pred_nat,
                                                   logits_pred, None, None,
-                                                  0.01, m, n)
+                                                  0.01, m, n, self.device)
 
             self.model.zero_grad()
             adv_loss = ot_loss
@@ -103,3 +103,87 @@ class FeaSAttack(nn.Module):
         return x
         # return logits_pred, adv_loss
 
+
+## OT Part
+
+def sinkhorn_loss_joint_IPOT(alpha, beta, x_feature, y_feature, x_label,
+                             y_label, epsilon, m, n, device):
+
+    C_fea = get_cost_matrix(x_feature, y_feature)
+    C = C_fea
+    T = sinkhorn(C, 0.01, 100, device)
+    # T = IPOT(C, 1)
+    batch_size = C.size(0)
+    cost_ot = torch.sum(T * C)
+    return cost_ot
+
+
+def sinkhorn(C, epsilon, niter=50, device='cuda'):
+    m = C.size(0)
+    n = C.size(1)
+    mu = Variable(1. / m * torch.FloatTensor(m).fill_(1).to(device),
+                  requires_grad=False)
+    nu = Variable(1. / n * torch.FloatTensor(n).fill_(1).to(device),
+                  requires_grad=False)
+
+    # Parameters of the Sinkhorn algorithm.
+    rho = 1  # (.5) **2          # unbalanced transport
+    tau = -.8  # nesterov-like acceleration
+    lam = rho / (rho + epsilon)  # Update exponent
+    thresh = 10**(-1)  # stopping criterion
+
+    # Elementary operations .....................................................................
+    def ave(u, u1):
+        "Barycenter subroutine, used by kinetic acceleration through extrapolation."
+        return tau * u + (1 - tau) * u1
+
+    def M(u, v):
+        "Modified cost for logarithmic updates"
+        "$M_{ij} = (-c_{ij} + u_i + v_j) / \epsilon$"
+        return (-C + u.unsqueeze(1) + v.unsqueeze(0)) / epsilon
+
+    def lse(A):
+        "log-sum-exp"
+        return torch.log(torch.exp(A).sum(1, keepdim=True) +
+                         1e-6)  # add 10^-6 to prevent NaN
+
+    # Actual Sinkhorn loop ......................................................................
+    u, v, err = 0. * mu, 0. * nu, 0.
+    actual_nits = 0  # to check if algorithm terminates because of threshold or max iterations reached
+
+    for i in range(niter):
+        u1 = u  # useful to check the update
+        u = epsilon * (torch.log(mu) - lse(M(u, v)).squeeze()) + u
+        v = epsilon * (torch.log(nu) - lse(M(u, v).t()).squeeze()) + v
+        # accelerated unbalanced iterations
+        # u = ave( u, lam * ( epsilon * ( torch.log(mu) - lse(M(u,v)).squeeze()   ) + u ) )
+        # v = ave( v, lam * ( epsilon * ( torch.log(nu) - lse(M(u,v).t()).squeeze() ) + v ) )
+        err = (u - u1).abs().sum()
+
+        actual_nits += 1
+        if (err < thresh).cpu().data.numpy():
+            break
+    U, V = u, v
+
+    pi = torch.exp(M(U, V))  # Transport plan pi = diag(a)*K*diag(b)
+    pi = pi.to(device).float()
+    return pi  # return the transport
+
+
+def get_cost_matrix(x_feature, y_feature):
+    C_fea = cost_matrix_cos(x_feature, y_feature)  # Wasserstein cost function
+    return C_fea
+
+
+def cost_matrix_cos(x, y, p=2):
+    # return the m*n sized cost matrix
+    "Returns the matrix of $|x_i-y_j|^p$."
+    # un squeeze differently so that the tensors can broadcast
+    # dim-2 (summed over) is the feature dim
+    x_col = x.unsqueeze(1)
+    y_lin = y.unsqueeze(0)
+
+    cos = nn.CosineSimilarity(dim=2, eps=1e-6)
+    c = torch.clamp(1 - cos(x_col, y_lin), min=0)
+
+    return c

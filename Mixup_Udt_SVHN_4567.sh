af421a24e355e0f953b489fae457487d913e87a0
GenBill <zhang478127826@gmail.com>
2022-06-18 10:59:27 +0800

    Uncert Mixup SVHN List

--- Mixup_Udt_SVHN_4567.sh
+++ Mixup_Udt_SVHN_4567.sh
@@ -1,21 +1,21 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10262 frost-adv-train-SVHN-Udt2.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10262 frost-adv-train-svhn-Udt2.py
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10264 frost-adv-train-SVHN-Udt4.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10264 frost-adv-train-svhn-Udt4.py
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10266 frost-adv-train-SVHN-Udt6.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10266 frost-adv-train-svhn-Udt6.py
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10268 frost-adv-train-SVHN-Udt8.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10268 frost-adv-train-svhn-Udt8.py
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10261 frost-adv-train-SVHN-Udt1.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10261 frost-adv-train-svhn-Udt1.py
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10263 frost-adv-train-SVHN-Udt3.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10263 frost-adv-train-svhn-Udt3.py
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10265 frost-adv-train-SVHN-Udt5.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10265 frost-adv-train-svhn-Udt5.py
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10267 frost-adv-train-SVHN-Udt7.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10267 frost-adv-train-svhn-Udt7.py
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10269 frost-adv-train-SVHN-Udt.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10269 frost-adv-train-svhn-Udt9.py
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10270 frost-adv-train-SVHN-Udt10.py
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10270 frost-adv-train-svhn-Udt10.py
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 10271 adv-Finaltest-svhn_Udt.py
